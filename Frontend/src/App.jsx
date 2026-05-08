8022cd4c089ba95688d896c06c0e75a3886f91be
codedriftlab <tempmail3470@gmail.com>
2026-05-08 13:34:38 +0530

    update home page & player page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VideoPlayer from "./pages/VideoPlayer";
 
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <BrowserRouter>
+      <Header onToggleSidebar={() => setIsOpen(!isOpen)} />
+
+      <Sidebar isOpen={isOpen} />
+
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/watch/:videoId" element={<Watch />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
+
         <Route path="/video/:id" element={<VideoPlayer />} />
       </Routes>
     </BrowserRouter>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 import VideoCard from "../components/VideoCard";
 import API from "../services/api";
 import "../styles/video.css";
 import "../styles/home.css";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(true);
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
@@ -17,18 +14,13 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-          <Header onToggleSidebar={() => setIsOpen(!isOpen)} />
-
-      <Sidebar isOpen={isOpen} />
-      <div className="home">
-        <div className="video-grid">
-          {videos.map((video) => (
-            <VideoCard key={video._id} video={video} />
-          ))}
-        </div>
+    <div className="home">
+      <div className="video-grid">
+        {videos.map((video) => (
+          <VideoCard key={video._id} video={video} />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
