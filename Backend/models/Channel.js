8ffd99edc99abef208a5a7bf257c2077a79947c1
codedriftlab <tempmail3470@gmail.com>
2026-05-08 12:02:46 +0530

    update channel  model file

---
+++ Backend/models/Channel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    channelBanner: {
+      type: String,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Channel", channelSchema);
